281a5ec931991269a55876b31e67d1de99fcc7a9
sbrunner <sbrunner@puppetlabs.vm>
2014-06-18 12:14:09 +0000

    Use defaults

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -13,30 +13,26 @@ class apache {
     require => Package['httpd'],
   }
 
-  file { '/var/www':
-    ensure => directory,
+  File {
     owner  => 'apache',
     group  => 'apache',
+    mode   => '0644',
+  }
+
+  file { '/var/www':
+    ensure => directory,
   }
 
   file { '/var/www/html':
     ensure => directory,
-    owner  => 'apache',
-    group  => 'apache',
   }
 
   file { '/var/www/html/index.html':
     ensure => file,
-    owner  => 'apache',
-    group  => 'apache',
-    mode   => '0644',
     source => 'puppet:///modules/apache/index.html',
   }
 
   file { '/etc/httpd/conf/httpd.conf':
-    owner  => '0',
-    group  => '0',
-    mode   => '0644',
     source => 'puppet:///modules/apache/httpd.conf',
     require => Package['httpd'],
   }
