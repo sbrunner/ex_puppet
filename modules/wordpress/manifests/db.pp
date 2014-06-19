09bb762bd3e379c42bef1b992c4dca42539bbed4
sbrunner <sbrunner@puppetlabs.vm>
2014-06-19 13:32:53 +0000

    Add required modules

---
+++ modules/wordpress/manifests/db.pp
@@ -0,0 +1,32 @@
+class wordpress::db (
+  $create_db,
+  $create_db_user,
+  $db_name,
+  $db_host,
+  $db_user,
+  $db_password,
+) {
+  validate_bool($create_db,$create_db_user)
+  validate_string($db_name,$db_host,$db_user,$db_password)
+
+  ## Set up DB using puppetlabs-mysql defined type
+  if $create_db {
+    mysql_database { $db_name:
+      charset => 'utf8',
+      require => Class['wordpress::app'],
+    }
+  }
+  if $create_db_user {
+    mysql_user { "${db_user}@${db_host}":
+      password_hash => mysql_password($db_password),
+      require       => Class['wordpress::app'],
+    }
+    mysql_grant { "${db_user}@${db_host}/${db_name}.*":
+      table      => "${db_name}.*",
+      user       => "${db_user}@${db_host}",
+      privileges => ['ALL'],
+      require    => Class['wordpress::app'],
+    }
+  }
+
+}
