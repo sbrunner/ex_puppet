f56f90708db024723d82061a599826e881d2fa7e
sbrunner <sbrunner@puppetlabs.vm>
2014-06-19 13:00:37 +0000

    Some tests

--- modules/cond/manifests/init.pp
+++ modules/cond/manifests/init.pp
@@ -1,4 +1,19 @@
 class cond {
+    class { 'staging':
+      path  => '/var/staging',
+      owner => 'puppet',
+      group => 'puppet',
+    }
+    ::staging::file { 'apache-tomcat-6.0.41.tar.gz':
+      source => 'http://apache.cs.utah.edu/tomcat/tomcat-6/v6.0.41/bin/apache-tomcat-6.0.41-deployer.tar.gz',
+    }
+    ::staging::extract { 'apache-tomcat-6.0.41.tar.gz':
+      target  => '/var/staging',
+      creates => '/var/staging/apache-tomcat-6.0.41',
+      require => Staging::File['apache-tomcat-6.0.41.tar.gz'],
+    }
+
+  notice(abs(-42.42))
   case $::osfamily {
     'debian': {
       $qualif = 'the best'
